830bf19d99b0360ab26c909d2a3d4ff0783db4d0
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-07 21:12:32 +0200

    Zakończenie zadania 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,7 +6,19 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj liczbę całkowitą: ");
+            int intNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if ((intNumber % 2) != 0)
+            {
+                Console.WriteLine($"{intNumber} jest liczbą nieparzystą");
+            }
+            else
+            {
+                Console.WriteLine($"{intNumber} jest liczbą parzystą");
+            }
         }
     }
 }
