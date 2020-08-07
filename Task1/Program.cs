4a6a7443b238e4ea1a78d5ea0bf2638dc2bf8340
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-07 21:00:14 +0200

    Zakończenie zadania 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,13 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            int a = 5;
+            int b = 5;
+
+            if (a == b)
+            {
+                Console.WriteLine("5 i 5 są równe");
+            }
         }
     }
 }
