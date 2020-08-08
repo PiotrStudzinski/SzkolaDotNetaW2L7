f64a764e546842363c6e12491da0e8d5c5fcea68
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-08 20:24:23 +0200

    Zakończenie zadania 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -6,7 +6,27 @@ namespace Task7
     {
         static void Main(string[] args)
         {
+            Console.Write("Podaj pierwszą liczbę: ");
+            int numOne = int.Parse(Console.ReadLine());
 
+            Console.Write("Podaj drugą liczbę: ");
+            int numTwo = int.Parse(Console.ReadLine());
+
+            Console.Write("Podaj trzecią liczbę: ");
+            int numThree = int.Parse(Console.ReadLine());
+
+            if (numOne > numTwo && numOne > numThree)
+            {
+                Console.WriteLine($"{numOne} jest największą liczbą z podanych");
+            }
+            else if (numTwo > numOne && numTwo > numThree)
+            {
+                Console.WriteLine($"{numTwo} jest największą liczbą z podanych");
+            }
+            else
+            {
+                Console.WriteLine($"{numThree} jest największą liczbą z podanych");
+            }
         }
     }
 }
