7e1d3d47c8b4d74c62f02f5a91e9f64315146476
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-08 18:58:12 +0200

    Zakończenie zadania 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -6,7 +6,36 @@ namespace Task5
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj swój wiek: ");
+            byte age = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // nie znalazłem informacji o minimalnym wieku na premiera :)
+            if (age >= 21)
+            {
+                Console.WriteLine("Możesz zostać posłem.");
+
+                if (age >= 30)
+                {
+                    Console.WriteLine("lub też");
+                    Console.WriteLine("Możesz zostać senatorem.");
+                    
+                    if (age >= 35)
+                    {
+                        Console.WriteLine("lub też");
+                        Console.WriteLine("Możesz zostać prezydentem.");
+                    }
+                }
+            }
+            else if ((age < 21) && (age >= 18))
+            {
+                Console.WriteLine("Możesz już głosować, ale nie możesz zostać posłem, premierem, senatorem i prezydentem.");
+            }
+            else
+            {
+                Console.WriteLine("Nie możesz głosować i nie możesz zostać posłem, premierem, senatorem i prezydentem.");
+            }
         }
     }
 }
