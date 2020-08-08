a02f6bba738042afc91a8043865a909a48666d1c
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-08 18:10:33 +0200

    Zakończenie zadania 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,7 +6,19 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj rok:");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                Console.WriteLine($"{year} jest rokiem przestępnym.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} nie jest rokiem przestępnym.");
+            }
         }
     }
 }
