e2cd7c0e5c660b061de8e832c015529468c49ea4
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-08 17:36:56 +0200

    Zakończenie zadania 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,7 +6,23 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj dowolną liczbę: ");
+            double number = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (number > 0)
+            {
+                Console.WriteLine("Podana liczba jest liczbą dodatnią.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Podana liczba jest liczbą ujemną.");
+            }
+            else
+            {
+                Console.WriteLine("Podana liczba zerem.");
+            }
         }
     }
 }
