d70c8d30931832f39d2cd08563b1f1508d3944ee
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 13:46:50 +0200

    Zakończenie zadania 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,7 +6,47 @@ namespace Task13
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj pierwszą liczbę: ");
+            float numFirst = float.Parse(Console.ReadLine());
+
+            Console.Write("Podaj drugą liczbę: ");
+            float numSecond = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Menu");
+            Console.WriteLine("--------------");
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie");
+
+            Console.WriteLine();
+
+            Console.Write("Podaj numer operacji do wykonania: ");
+            int menu = int.Parse(Console.ReadLine());
+
+            float result = 0;
+
+            switch (menu)
+            {
+                case 1:
+                    result = numFirst + numSecond;
+                    break;
+                case 2:
+                    result = numFirst - numSecond;
+                    break;
+                case 3:
+                    result = numFirst * numSecond;
+                    break;
+                case 4:
+                    result = numFirst / numSecond;
+                    break;
+            }
+
+            Console.WriteLine();
+
+            Console.Write($"Twój wynik to: {result}.");
         }
     }
 }
