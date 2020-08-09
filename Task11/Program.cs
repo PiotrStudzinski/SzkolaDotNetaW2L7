84ca572e22a06e9b0c56b970734335685fe034a5
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 12:57:16 +0200

    Zakończenie zadania 11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 
 namespace Task11
 {
@@ -6,7 +7,33 @@ namespace Task11
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj ocenę: ");
+            int schoolGrade = int.Parse(Console.ReadLine());
+
+            switch(schoolGrade)
+            {
+                case 1:
+                    Console.WriteLine("Niedostateczny");
+                    break;
+                case 2:
+                    Console.WriteLine("Dopuszczający");
+                    break;
+                case 3:
+                    Console.WriteLine("Dostateczny");
+                    break;
+                case 4:
+                    Console.WriteLine("Dobry");
+                    break;
+                case 5:
+                    Console.WriteLine("Bardzo dobry");
+                    break;
+                case 6:
+                    Console.WriteLine("Celujący");
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiej oceny");
+                    break;
+            }
         }
     }
 }
