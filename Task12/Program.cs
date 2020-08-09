a612768f8f49f7283bc7696393140865fb9ae5f0
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 13:14:27 +0200

    Zakończenie zadania 12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -6,7 +6,33 @@ namespace Task12
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj liczbę (1-7): ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Poniedziałek");
+                    break;
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("Środa");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+                case 5:
+                    Console.WriteLine("Piątek");
+                    break;
+                case 6:
+                    Console.WriteLine("Sobota");
+                    break;
+                case 7:
+                    Console.WriteLine("Niedziela");
+                    break;
+            }
         }
     }
 }
