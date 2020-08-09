9bc1bad91586fe9950b5e527fa2a6fc2bc1d0718
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 12:42:06 +0200

    Zakończenie zadania 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -6,7 +6,18 @@ namespace Task10
     {
         static void Main(string[] args)
         {
-            
+            int sideOne =40;
+            int sideTwo = 55;
+            int sideThree = 65;
+
+            if(((sideOne + sideTwo) > sideThree) && ((sideOne + sideThree) > sideTwo) && ((sideTwo + sideThree) > sideOne))
+            {
+                Console.WriteLine("Można zbudować trójkąt");
+            }
+            else
+            {
+                Console.WriteLine("Nie można zbudować trójkąta");
+            }
         }
     }
 }
