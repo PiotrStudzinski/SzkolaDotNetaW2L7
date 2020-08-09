a94731e3f970e65fa7b916c5af7d3b1838df1f7f
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 11:59:05 +0200

    Zakończenie zadania 9

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -6,7 +6,35 @@ namespace Task9
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj aktualną temperaturę: ");
+            int temperature = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (temperature < 0)
+            {
+                Console.WriteLine("cholernie piździ");
+            }
+            else if (temperature >= 0 && temperature < 10)
+            {
+                Console.WriteLine("zimno");
+            }
+            else if (temperature >= 10 && temperature < 20)
+            {
+                Console.WriteLine("chłodno");
+            }
+            else if (temperature >= 20 && temperature < 30)
+            {
+                Console.WriteLine("w sam raz");
+            }
+            else if (temperature >= 30 && temperature < 40)
+            {
+                Console.WriteLine("w sam raz");
+            }
+            else
+            {
+                Console.WriteLine("a weź wyprowadzam się na Alaskę");
+            }
         }
     }
 }
