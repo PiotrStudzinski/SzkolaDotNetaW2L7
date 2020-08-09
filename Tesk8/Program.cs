80e2b051c47026658d77bc47feaadf8d36d10200
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 11:38:01 +0200

    Zakończanie zadania 8

--- Tesk8/Program.cs
+++ Tesk8/Program.cs
@@ -6,7 +6,18 @@ namespace Tesk8
     {
         static void Main(string[] args)
         {
-            
+            int maths = 80;
+            int physics = 71;
+            int chemistry = 0;
+
+            if((maths >= 70 && physics >= 55 && chemistry >= 45) || (maths + physics >= 150) || (maths + chemistry >= 150))
+            {
+                Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+            }
+            else
+            {
+                Console.WriteLine("Kandydat nie jest dopuszczony do rekrutacji");
+            }
         }
     }
 }
